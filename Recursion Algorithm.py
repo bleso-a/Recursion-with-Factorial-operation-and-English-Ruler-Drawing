eb22b6f75adde43febbdac7789a47dd4c609bd50
Blessing Adetoye Adesiji <38926059+bleso-a@users.noreply.github.com>
2020-01-17 09:48:41 +0100

    Update Recursion Algorithm.py

--- Recursion Algorithm.py
+++ Recursion Algorithm.py
@@ -10,21 +10,21 @@ class EnglishRuler:
         self.__num_inches = num_inches
         self.__major_length = major_length
     
-    
+    #draw line method
     def draw_line(self, tick_length, tick_label=''):
         line ='-'*tick_length
         if tick_label:
             line += ''+tick_label
         print(line)
         
-        
+    #draw interval method    
     def draw_interval(self, center_length):
         if center_length>0:
             self.draw_interval(center_length - 1)
             self.draw_line(center_length)
             self.draw_interval(center_length - 1)
             
-            
+    #draw ruler method        
     def draw_ruler(self):
         self.draw_line(self.__major_length, '0')
         for j in range(1, 1+self.__num_inches):
@@ -33,5 +33,5 @@ class EnglishRuler:
             
 
 if __name__ == '__main__':
-    ruler = EnglishRuler(5,5)
+    ruler = EnglishRuler(4,5)
     ruler.draw_ruler()
